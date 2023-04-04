02d930dffe1ce05ebd71f53845438a6961c47ece
Pavan <pavandakore@gmail.com>
2023-04-04 14:12:21 +0530

    updated version

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     var User = null 
 
   return (
-    <nav>
+    <nav className='main-nav'>
         <div className="navbar">
             {/* we will add various tabs 
             of navbar such as home about us ,, using the link element*/}
