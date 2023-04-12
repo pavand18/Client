6c3d56ac52c468d9c7984f4d79e76d03c0fb86e6
Pavan <pavandakore@gmail.com>
2023-04-12 16:40:32 +0530

    restarted the work

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 import '../../App.css'
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
-import RightSidebar from '../../components/RightSidebar/RightSidebar'
-import HomeMainbar from '../../components/HomeMainbar/HomeMainbar'
+// import RightSidebar from '../../components/RightSidebar/RightSidebar'
+// import HomeMainbar from '../../components/HomeMainbar/HomeMainbar'
 
 const Home = () => {
     return (
         <div className='home-container-1'>
             <LeftSidebar />
             <div className='home-container-2'>
-                <HomeMainbar />
-                <RightSidebar />
+                {/* <HomeMainbar /> */}
+                {/* <RightSidebar /> */}
             </div>
         </div>
     )
 }
 
-export default Home
+export default Home     
+
+
